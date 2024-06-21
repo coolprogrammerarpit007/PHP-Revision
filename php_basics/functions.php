cdc58f0fc48f2b6fca5452e655fbc5884c86922b
arpit mishra <arpit.mishra.out@gmail.com>
2024-06-21 11:12:53 +0530

    functions in php

---
+++ php_basics/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+
+    // Built In Functions in PHP
+
+    // var_dump(round(7.8)). "<br>";
+    // var_dump(ceil(8.2)). "<br>";
+    // var_dump(floor(9.9)). "<br>";
+    // var_dump(rand(25,75));
+    // var_dump(rand(25,75));
+    // var_dump(min(78,25,12,-855,125));
+    // var_dump(max(78,25,12,-855,125));
+
+    // $number = NAN;
+    // echo is_nan($number);
